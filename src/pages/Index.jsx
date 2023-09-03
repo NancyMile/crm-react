5ee20af4dce299cde1b55300b231d9982745ee42
nancy <smithnancy@outlook.com>
2023-09-03 22:32:08 +1000

    iterating on clients and displaying info

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import Client from "../components/Client";
 
 export function loader() {
   const clients = [
@@ -50,6 +51,30 @@ const Index = () => {
     <>
       <h1 className="font-black text-4xl text-blue-900">Clients</h1>
       <p className="mt-3 ">Manage Clients</p>
+
+      {clients.length ? (
+        //has clients
+        <table className="w-full bg-white shadow mt-5 table-auto">
+          <thead className="bg-blue-800 text-white ">
+            <tr>
+              <th className="p-2">Client</th>
+              <th className="p-2">Contact</th>
+              <th className="p-2">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clients.map(client => (
+              <Client
+                key={client.id}
+                client={client}
+              />
+            ))}
+          </tbody>
+        </table>
+
+      ) : (
+          <p>No clients</p>
+      )}
     </>
   )
 }
