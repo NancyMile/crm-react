de1bd1600855938e264a5fd28de6acdfafb5a463
nancy <smithnancy@outlook.com>
2023-09-03 16:56:52 +1000

    hightlight actual page on link

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,18 @@
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet, Link, useLocation } from 'react-router-dom'
 const Layout = () => {
+
+  const location = useLocation();
+
+  console.log(location)
+
   return (
       <div className='md:flex  md:min-h-screen'>
         <aside className='md:w-1/4 bg-blue-900 px-5 py-10'>
           <h2 className='text-4xl text-white font-black text-center'>CRM -Clients</h2>
 
           <div className='mt-10'>
-          <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to="/">Home</Link>
-          <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to="/clients/new">New Client</Link>
+          <Link className={`${location.pathname === '/' ? 'text-blue-300': 'text-white'} text-2xl block mt-2 hover:text-blue-300`} to="/">Home</Link>
+          <Link className={`${location.pathname === '/clients/new' ? 'text-blue-300': 'text-white'} text-2xl block mt-2 hover:text-blue-300`} to="/clients/new">New Client</Link>
           </div>
         </aside>
         <main className='md:w-3/4 p-10 md:h-screen overflow-scroll'>
