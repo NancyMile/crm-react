ed99e60afdfd8b8f2c349e36ff4cba09dd6a5322
nancy <smithnancy@outlook.com>
2023-09-03 22:10:38 +1000

    getting data form loader with userLoaderData

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,51 @@
+import { useLoaderData } from "react-router-dom";
+
 export function loader() {
-  return 'Desde loader';
+  const clients = [
+    {
+        id: 1,
+        name: 'Jess',
+        phone: 102013313,
+        email: "Jess@sky.com",
+        company: 'Sky Ltda'
+    },
+    {
+        id: 2,
+        name: 'Karen',
+        phone: 138198313,
+        email: "karen@sky.com",
+        company: 'Sky Ltda'
+    },
+    {
+        id: 3,
+        name: 'Josue',
+        phone: 31983913,
+        email: "josue@sky.com",
+        company: 'Sky Ltda'
+    },
+    {
+        id: 4,
+        name: 'Miguel',
+        phone: 319381983,
+        email: "miguel@sky.com",
+        company: 'Sky Ltda'
+    },
+    {
+        id: 5,
+        name: 'Pedro',
+        phone: 1398198938,
+        email: "pedro@sky.com",
+        company: 'Sky Ltda'
+    },
+];
+  return  clients;
  }
 
 const Index = () => {
+
+  const clients = useLoaderData();
+  //console.log(clients);
+
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Clients</h1>
