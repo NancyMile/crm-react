acb2fc3d749074a6cc2d7e1395cd6faf3ad8f2f9
nancy <smithnancy@outlook.com>
2023-09-04 21:06:36 +1000

    validation added to new client form

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+const Error = ({ children }) => {
+  return (
+      <div className=" text-center my-4 uppercase font-bold bg-red-600 text-white p-3 ">
+          {children}    
+      </div>
+  )
+}
+
+export default Error
--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,23 +1,33 @@
-import {useNavigate, Form} from 'react-router-dom'
+import {useNavigate, Form, useActionData} from 'react-router-dom'
 import ClientForm from '../components/ClientForm'
+import Error from '../components/Error'
 
 export async function action({ request }) {
-  //console.log("form action")
-  //console.log(request)
   const formData = await request.formData()
-  //console.log(formData.get('name'))
-  //console.log([...formData]) //gest all data
-
-  //better option for getting data
   const data = Object.fromEntries(formData)
-  console.log(data)
+  //console.log(data)
+
+  //validate data
+  const errors = []
+  if (Object.values(data).includes('')) {
+    errors.push('All the fields are compulsory')
+  }
+  //console.log(errors)
+
+  //return errors
+  if (Object.keys(errors).length) {
+    return errors
+  }
+
   return 1
 }
 
 const NewClient = () => {
 
   const navigate = useNavigate()
+  const errors = useActionData()
 
+  console.log(errors)
   return (
     <>
        <h1 className="font-black text-4xl text-blue-900">New Client</h1>
@@ -30,6 +40,10 @@ const NewClient = () => {
         </button>
       </div>
       <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+        {errors?.length && errors.map((error, i) => (
+          <Error  key={i}>{error}</Error>
+        ))}
+
         <Form
           method='post'
         >
