1f71fa2d5c58a7ed5e8dc26b3d2714a18fb0c494
nancy <smithnancy@outlook.com>
2023-09-04 11:11:27 +1000

    adding form for new client

--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import {useNavigate} from 'react-router-dom'
+import Form from '../components/Form'
 
 const NewClient = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>NewClient</div>
+    <>
+       <h1 className="font-black text-4xl text-blue-900">New Client</h1>
+      <p className="mt-3 ">Please fill the information</p>
+      <div className='flex justify-end'>
+        <button className='bg-blue-800 px-3 py-1 font-bold uppercase text-white'
+          onClick={() => navigate(-1)}
+        >
+          Back
+        </button>
+      </div>
+      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+        <form>
+          <Form />
+          <input className="bg-blue-800 p-3 w-full uppercase text-white font-bold text-lg" type="submit" value="Save Client"/>
+        </form>
+      </div>
+    </>
   )
 }
 
