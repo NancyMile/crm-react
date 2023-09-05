b0b5c5b1726db6ac9a79f9fe3137a352838ccc24
nancy <smithnancy@outlook.com>
2023-09-05 12:43:30 +1000

    adding the edit form loading client info

--- src/components/ClientForm.jsx
+++ src/components/ClientForm.jsx
@@ -12,6 +12,7 @@ const ClientForm = ({client}) => {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="Client name"
                     name="name"
+                    defaultValue={client?.name}
                 />
             </div>
             <div className="mb-4">
@@ -25,6 +26,7 @@ const ClientForm = ({client}) => {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="company"
                     name="company"
+                    defaultValue={client?.company}
                 />
             </div>
 
@@ -39,6 +41,7 @@ const ClientForm = ({client}) => {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="Email"
                     name="email"
+                    defaultValue={client?.email}
                 />
             </div>
 
@@ -53,6 +56,7 @@ const ClientForm = ({client}) => {
                     className="mt-2 block w-full p-3 bg-gray-50"
                     placeholder="phone"
                     name="phone"
+                    defaultValue={client?.phone}
                 />
             </div>
 
@@ -68,6 +72,7 @@ const ClientForm = ({client}) => {
                     className="mt-2 block w-full p-3 bg-gray-50 h-40 align-self"
                     placeholder="Notes"
                     name="notes"
+                    defaultValue={client?.notes}
                 />
             </div>
         </>
--- src/pages/EditClient.jsx
+++ src/pages/EditClient.jsx
@@ -1,4 +1,6 @@
+import { Form, useNavigate, useLoaderData } from "react-router-dom"
 import { editClient } from "../data/clients"
+import ClientForm from "../components/ClientForm"
 
 export async function loader({ params }) {
     //console.log(params)
@@ -15,9 +17,37 @@ export async function loader({ params }) {
 }
 
 const EditClient = () => {
-  return (
-    <div>EditClient</div>
-  )
+    const navigate = useNavigate()
+    const client = useLoaderData()
+    //console.log(client)
+    return (
+        <>
+            <h1 className="font-black text-4xl text-blue-900">Edit Client</h1>
+            <p className="mt-3 ">Please fill the information</p>
+            <div className='flex justify-end'>
+                <button className='bg-blue-800 px-3 py-1 font-bold uppercase text-white'
+                onClick={() => navigate(-1)}
+                >
+                Back
+                </button>
+            </div>
+            <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+                {/* {errors?.length && errors.map((error, i) => (
+                <Error  key={i}>{error}</Error>
+                ))} */}
+
+                <Form
+                method='post'
+                noValidate
+                >
+                <ClientForm
+                    client={client}
+                />
+                <input className="bg-blue-800 p-3 w-full uppercase text-white font-bold text-lg" type="submit" value="Save Changes"/>
+                </Form>
+            </div>
+        </>
+    )
 }
 
 export default EditClient
